d40f37170147b757cd41413c007a1ce1dbc9802a
EtSigfuss <etsigfuss@gmail.com>
2026-02-10 14:11:18 -0700

    function initiall

--- main-1.py
+++ main-1.py
@@ -1,10 +1,31 @@
 import xgboost as xgb
 from sklearn.datasets import make_regression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
-# Generate sample regression data
-X, y = make_regression(n_samples=1000, n_features=10, noise=0.1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Create XGBoost regressor
-xgb_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100)
+def shapefile_to_gdf (file):
+    """formats to shapefiles to geodata frame"""
+    pass
+
+def gdf_to_train_test_data(gdf):
+    """splits geodataframe to training and testing features"""
+    pass
+
+def train_model(model, X_train y_train ):
+    """
+    
+    """
+    pass
+
+#format data
+gdf = shapefile_to_gdf("Study Area.shp")
+#split to training and test data
+X_train, X_test, y_train, y_test = gdf_to_train_test_data(gdf)
+
+#initiallize models
+xgb_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100,)
+regressor = RandomForestRegressor(n_estimators=10, random_state=0, oob_score=True)
+
+#train model
+
