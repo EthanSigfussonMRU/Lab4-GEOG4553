c62ac7a96b5f5dc1457e8b8dff96ddb6216c2c57
EtSigfuss <etsigfuss@gmail.com>
2026-02-10 16:00:41 -0700

    small cleanup

--- main-1.py
+++ main-1.py
@@ -2,23 +2,19 @@ import warnings
 
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import seaborn as sns
-import sklearn
 import xgboost as xgb
-from sklearn.datasets import make_regression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.impute import KNNImputer
-from sklearn.metrics import f1_score, mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import (cross_val_predict, cross_val_score,
                                      train_test_split)
-from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 warnings.filterwarnings('ignore')
 
 
 def train_validate_and_predict(model, X, y):
+    model.fit()
+
     #cross validation scores
     cv_scores = cross_val_score(model, X, y, cv=5, scoring='r2')
     print(f"Scores: for {model=}{cv_scores}")
@@ -28,7 +24,7 @@ def train_validate_and_predict(model, X, y):
     y_pred = cross_val_predict(model, X, y, cv=5)
     print(f"\n--- XGBoost Results ---")
     
-    #score validation
+    #validation performance
     mse = mean_squared_error(y, y_pred)
     r2 = r2_score(y, y_pred)
     
