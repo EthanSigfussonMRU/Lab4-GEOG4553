5f754848e0128042c69492db661b08b49718053a
EtSigfuss <etsigfuss@gmail.com>
2026-02-10 16:02:17 -0700

    mooore cleanup

--- main-1.py
+++ main-1.py
@@ -63,9 +63,13 @@ gdf = gpd.read_file(".\\Shapefiles\\Study Area.shp")
 # print(gdf.head())
 # print(gdf.info())
 
-
+#1) Bring the GIS data into Python
 X = gdf.iloc[:,1:2].values
 y = gdf.iloc[:,2].values
+
+
+
+#2) Train and validate models on “Study Area”
 
 #initialize models
 rf_model = RandomForestRegressor(n_estimators=100, random_state=0)
@@ -74,51 +78,4 @@ xgb_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, lea
 train_validate_and_predict(rf_model,X,y)
 train_validate_and_predict(xgb_model,X,y)
 
-
-# #cross validation scores
-# rf_cv_scores = cross_val_score(rf_model, X, y, cv=5, scoring='r2')
-# xgb_cv_scores = cross_val_score(xgb_model, X, y, cv=5, scoring='r2')
-
-
-# print(f"Scores: {rf_cv_scores}")
-# print(f"Mean: {rf_cv_scores.mean():.2f}")
-
-# print(f"Scores: {xgb_cv_scores}")
-# print(f"Mean: {xgb_cv_scores.mean():.2f}")
-
-# #cross val predictions
-# y_rf_pred = cross_val_predict(rf_model, X, y, cv=5)
-# y_xgb_pred = cross_val_predict(xgb_model, X, y, cv=5)
-
-# #score validation
-# mse_xgb = mean_squared_error(y, y_xgb_pred)
-# r2_xgb = r2_score(y, y_xgb_pred)
-
-# print(f"\n--- XGBoost Results ---")
-# print(f"MSE: {mse_xgb:.4f}")
-# print(f"R2 Score: {r2_xgb:.4f}")
-
-# #residual graphing
-# res_rf = y - y_rf_pred
-# res_xgb = y - y_xgb_pred
-
-
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6), sharey=True)
-# # Random Forest Plot
-# sns.scatterplot(x=y_rf_pred, y=res_rf, ax=ax1, alpha=0.5, color='steelblue')
-# ax1.axhline(0, color='red', linestyle='--')
-# ax1.set_title('Random Forest Residuals')
-# ax1.set_xlabel('Predicted Values')
-# ax1.set_ylabel('Residuals')
-
-# # XGBoost Plot
-# sns.scatterplot(x=y_xgb_pred, y=res_xgb, ax=ax2, alpha=0.5, color='darkorange')
-# ax2.axhline(0, color='red', linestyle='--')
-# ax2.set_title('XGBoost Residuals')
-# ax2.set_xlabel('Predicted Values')
-
-# plt.tight_layout()
-# plt.show()
-
-
-# gdf_all_tracts = gpd.read_file(".\\Shapefiles\\All Census Tracts.shp")
+#3) Predict for “All Census Tracts” and evaluate testing performance
