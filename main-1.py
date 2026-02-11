38f593ac56514b6ed4b8af2367ae345976bc2c87
EtSigfuss <etsigfuss@gmail.com>
2026-02-11 11:34:06 -0700

    largely done

--- main-1.py
+++ main-1.py
@@ -14,33 +14,48 @@ warnings.filterwarnings('ignore')
 
 def train_validate_and_predict(model, X, y):
     """
-    trains a m
+    trains a model using 5 fold cross validation
+    reports validation performance to terminal.
+    And, genrates residual graph.
     """
 
     #get model name
     model_name = type(model).__name__
 
-    #cross validation scores
-    cv_scores = cross_val_score(model, X, y, cv=5, scoring='r2')
-    print(f"Cross validation scores: for {model_name}\n{cv_scores}")
-    print(f"Mean: {cv_scores.mean():.2f}")
+    #split to test and train
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    # #cross validation scores
+    # cv_scores = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
+    # print(f"Cross validation scores: for {model_name}\n{cv_scores}")
+    # print(f"Mean: {cv_scores.mean():.2f}")
 
     #cross val predictions
-    y_pred = cross_val_predict(model, X, y, cv=5)
+    y_pred = cross_val_predict(model, X_train, y_train, cv=5)
 
     #validation performance
-    mse = mean_squared_error(y, y_pred)
-    r2 = r2_score(y, y_pred)
+    mse = mean_squared_error(y_train, y_pred)
+    r2 = r2_score(y_train, y_pred)
     
     print(f"\n--- {model_name} validation performance ---")
     print(f"MSE: {mse:.4f}")
     print(f"R2 Score: {r2:.4f}")
 
 
+
+    #holdout test
+    model.fit(X_train, y_train)
+    model_score = model.score(X_test, y_test)
+    print(f"holdout score for {model_name} is {model_score}")
+
+    #final fit
+    model.fit(X,y)
+
+
     #residual graphing
     print("Graph saved in Figures, close graph to advance program\n")
 
-    res = y - y_pred
+    res = y_train - y_pred
 
     fig, ax1= plt.subplots(figsize=(15, 6), sharey=True)
 
@@ -56,6 +71,28 @@ def train_validate_and_predict(model, X, y):
 
 
     plt.savefig(f".\\Figures\\{model_name}_residuals.png")
+    plt.show()
+
+
+
+def predict_BUILD_LOSS_RATE(model, X):
+    """
+    Predicts the unknown build Loss rate of Census Tracts
+    and produces a histogram predicted loss rates
+    """
+    #get model name
+    model_name = type(model).__name__
+    pred_y = model.predict(X)
+
+    plt.hist(pred_y, bins=30, color="#FF00DD", edgecolor='black')
+
+    plt.title(f"BUILD_LOSS_RATE(Predicted) dist by {model_name}")
+    plt.xlabel("BUILD_LOSS_RATE(predicted)")
+    plt.ylabel("Count")
+
+    plt.savefig(f".\\Figures\\BUILD_LOSS_RATE(Predicted) dist by {model_name}.png")
+    print("Graph saved in Figures, close graph to advance program\n")
+    
     plt.show()
 
 
@@ -79,13 +116,18 @@ y = gdf['BUILD_LOSS'].values
 #2) Train and validate models on “Study Area”
 
 #initialize models
-rf_model = RandomForestRegressor(n_estimators=100, random_state=0)
-xgb_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1)
+rf_model = RandomForestRegressor(n_estimators=1000, random_state=0)
+xgb_model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=1000, learning_rate=0.1)
 
 train_validate_and_predict(rf_model,X,y)
 train_validate_and_predict(xgb_model,X,y)
 
 #3) Predict for “All Census Tracts” and evaluate testing performance
 
+
+#format data
 gdf_all = gpd.read_file(".\\Shapefiles\\All Census Tracts.shp")
 X_all = gdf_all[features].values
+
+predict_for_unknown_X(rf_model, X_all)
+predict_for_unknown_X(xgb_model, X_all)
